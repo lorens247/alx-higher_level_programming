6ac925f2d11880f59a7808e45b20388bd667701a
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-09-04 07:57:00 -0700

    python network 1

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A module that reads and prints the 10 most recent commits
+from a given repository using 2 arguments.
+"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        commits = response.json()
+
+        for commit in commits[:10]:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f'{sha}: {author_name}')
+    else:
+        pass
